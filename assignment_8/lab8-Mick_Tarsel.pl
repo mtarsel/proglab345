13acb78dc8951d582b0e1df03950d048e7d0300a
Mick Tarsel <mtarsel@gmail.com>
2013-12-06 10:51:14 -0500

    Completed lab8

--- assignment_8/lab8-Mick_Tarsel.pl
+++ assignment_8/lab8-Mick_Tarsel.pl
@@ -1,11 +1,8 @@
-% Maurice Materise
-% lab 8
+%Mick Tarsel
 
-% Extra functions needed
-
-append([],Bs,Bs).
-append([A|As],Bs,[A|Cs]):-
-append(As,Bs,Cs).
+%helpers
+member(A, [A|As]).
+member(A, [B|As]):- \+ A=B, member(A,As).
 
 reverse([],[]).
 reverse([A|As],Bs):-reverse(As,Xs),append(Xs,[A],Bs).
@@ -13,15 +10,14 @@ reverse([A|As],Bs):-reverse(As,Xs),append(Xs,[A],Bs).
 prefix([], Bs).
 prefix([A|As], [A|Bs]) :- prefix(As, Bs).
 
-member(A, [A|As]).
-member(A, [B|As]):- \+ A=B, member(A,As).
+append([],Bs,Bs).
+append([A|As],Bs,[A|Cs]):-
+append(As,Bs,Cs).
 
 delete_all(A,[],[]).
 delete_all(A,[A|As],Bs):-delete_all(A,As,Bs).
 delete_all(A,[B|As],Bs):-
 	\+ A=B, delete_all(A,As,Bs).
-
-
 
 % 1) solution without member
 
@@ -31,8 +27,6 @@ remove_duplicates([A|As],Bs):-
 remove_duplicates([A|As],[A|Bs]):-
 	\+ member(A,As), remove_duplicates(As,Bs).
 
-
-
 % 2) solution using member
 
 remove_duplicates_two([],[]).
@@ -41,23 +35,15 @@ remove_duplicates_two([A|As], Bs) :-
 remove_duplicates_two([A|As],[A|Bs]):- 
 	\+ member(A,As), remove_duplicates_two(As,Bs).
 
-
-
 % 3) one-liner using prefix
 
 suffix(As, Bs) :- reverse(As, Xs), reverse(Bs, Ys), prefix(Xs, Ys).
-
-
 
 % 4) two-liner without prefix
 
 suffix_two(As, As).
 suffix_two(As, [B|Bs]):-suffix(As,Bs).
 
-
-
 % 4) one-liner without prefix
 
 suffix_three(As,Bs):-append(_,As,Bs).
-	
-
